c0c285e78b870dfc1ee74f754e44cc63ef1e94d3
marcosferr <ferreiramarcosadrian@gmail.com>
2023-06-26 14:53:36 -0400

    Add contact and menuPage with elements

--- src/pages/contactPage.js
+++ src/pages/contactPage.js
@@ -1,15 +1,25 @@
-const homePage = document.createElement("div");
+import mapImg from "../img/restaurant-location.png";
+const contactPage = document.createElement("div");
 const container = document.createElement("div");
-const message1 = document.createElement("div");
-const message2 = document.createElement("div");
-const cheffImage = document.createElement("img");
-const message3 = document.createElement("div");
+
+container.classList.add("container");
+
+const message1 = document.createElement("p");
+message1.innerHTML = "Call us: 917 893 222";
+message1.style.alignSelf = "flex-start";
+message1.style.padding = "30px";
+const message2 = document.createElement("p");
+message2.innerHTML = "Hollywood Boulevard 42, Los Angeles, USA";
+message2.style.alignSelf = "flex-start";
+message2.style.padding = "30px";
+const mapImage = document.createElement("img");
+mapImage.src = mapImg;
+mapImage.style.maxWidth = "100%";
 
 //Get all together
-homePage.appendChild(container);
+contactPage.appendChild(container);
 container.appendChild(message1);
 container.appendChild(message2);
-container.appendChild(cheffImage);
-container.appendChild(message3);
+container.appendChild(mapImage);
 
-export default homePage;
+export default contactPage;
--- src/pages/menuPage.js
+++ src/pages/menuPage.js
@@ -1,17 +1,26 @@
-const menuPage = document.createElement("div");
-const homePage = document.createElement("div");
-const container = document.createElement("div");
-const message1 = document.createElement("div");
-const message2 = document.createElement("div");
-const cheffImage = document.createElement("img");
+import pizzaCard from "../img/pizza.png";
+const cardData = {
+  image: pizzaCard,
+  title: "Pizza Mambaloni",
+  description: "The most selected pizza by our customers",
+};
+const cardContainer = document.createElement("div");
+cardContainer.classList.add("container");
+const cardImage = document.createElement("img");
+cardImage.src = cardData.image;
+cardImage.style.width = "100%";
+cardContainer.appendChild(cardImage);
 
-const message3 = document.createElement("div");
+const cardTitle = document.createElement("h2");
+cardTitle.textContent = cardData.title;
+cardTitle.style.color = "#FFF";
+cardTitle.style.textAlign = "center";
+cardContainer.appendChild(cardTitle);
 
-//Get all together
-homePage.appendChild(container);
-container.appendChild(message1);
-container.appendChild(message2);
-container.appendChild(cheffImage);
-container.appendChild(message3);
+const cardDescription = document.createElement("p");
+cardDescription.textContent = cardData.description;
+cardDescription.style.color = "#FFF";
+cardDescription.style.textAlign = "center";
+cardContainer.appendChild(cardDescription);
 
-export default homePage;
+export default cardContainer;
